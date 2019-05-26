5db16d5a498837685d873e8df2d1763c273c8102
darco2018 <newsecond@outlook.com>
2019-05-26 14:29:12 +0200

    Remove pragma from prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 /** @format */
 // dont pass options to Prettier via your ESLint configuration file - VS can't read it then
 module.exports = {
-  insertPragma: true, //not working with eslint-loader
+  insertPragma: false, //not working with eslint-loader
   quoteProps: "consistent"
 
   // tabWidth: 4  >> change for testing purposes
