ce2508186db905916ff30a39fe67e8e977f6eb8d
darco2018 <newsecond@outlook.com>
2019-06-03 14:22:18 +0200

    Remove details that hindered quick start

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+//j is lowercase: jQuery
+if (jQuery) {
+    $("body").append("<br>Jquery is loaded.");
+} else {
+    alert("Jquery is NOT loaded");
+}
--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("two").innerHTML = "JS is working";
-
-//j is lowercase: jQuery
-if (jQuery) {
-    $("#two").append("<br>Jquery is loaded - from CDN");
-} else {
-    alert("Jquery is NOT loaded");
-}
