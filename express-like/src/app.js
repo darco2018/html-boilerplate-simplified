7d6290d888491c7c9e869f619075e4ad42c92388
darco2018 <newsecond@outlook.com>
2019-06-21 11:41:27 +0200

    Minor changes in app.js & vendor.js

--- express-like/src/app.js
+++ express-like/src/app.js
@@ -3,7 +3,9 @@ import "./public/stylesheets/scss/main.scss";
 import { AlertService } from "./public/javascripts/alert.service";
 import { ComponentService } from "./public/javascripts/component.service";
 
+console.log("app.js is calling...");
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
-console.log("J");
+
--- express-like/src/public/javascripts/vendor/vendor.js
+++ express-like/src/public/javascripts/vendor/vendor.js
@@ -1,6 +1,16 @@
 import "bootstrap"; // imported from node modules
 
 import $ from "jquery"; // imported from node modules
+
+/* NOT WORKING:
+import "./jquery"
+import jQuery from "./jquery"
+console.log($("body").length); */
+
+// local imports CANNOT have 2 dots in the name when syntax: import <filename>
+// Perhaps its ok with require & its ok with import { <name> } from <filename>
+// import "./jquery-2.2.4-min"; import from the same folder
+
 console.log("This doesnt have to be called or imported to run...");
 console.log(`JQuery is working in vendor.js if non-zero value: ${$("body").length}`);
 
